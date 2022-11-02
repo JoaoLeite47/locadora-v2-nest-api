b8cb72894b7da8e010430fabfcd34ddce7f344af
JoaoLeite47 <StalkerJJ@outlook.com>
2022-11-02 17:52:15 -0300

    feat<loja> delete endpoint has implemented

--- src/loja/loja.controller.ts
+++ src/loja/loja.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateLojaDto } from './dto/create-loja.dto';
 import { LojaService } from './loja.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger/dist/decorators';
 import { Loja } from './entities/loja.entity';
+import { UpdateLojaDto } from './dto/update-loja.dto';
 
 @ApiTags('Loja')
 @Controller('loja')
@@ -10,6 +21,9 @@ export class lojaController {
   constructor(private lojaService: LojaService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Visualisar todas as lojas ',
+  })
   findAll() {
     return this.lojaService.findAll();
   }
@@ -23,7 +37,27 @@ export class lojaController {
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Cadastrar uma loja ',
+  })
   create(@Body() createLojaDto: CreateLojaDto) {
     return this.lojaService.create(createLojaDto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Editar uma loja pelo ID',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateLojaDto): Promise<Loja> {
+    return this.lojaService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT) //quando não se retorna nada em uma req, por padrão ela pode retornar 204
+  @ApiOperation({
+    summary: 'Remover uma loja pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    this.lojaService.delete(id);
   }
 }
--- src/loja/loja.service.ts
+++ src/loja/loja.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateLojaDto } from './dto/create-loja.dto';
+import { UpdateLojaDto } from './dto/update-loja.dto';
 import { Loja } from './entities/loja.entity';
 
 @Injectable()
@@ -19,4 +20,17 @@ export class LojaService {
     const data: Loja = { ...dto };
     return this.prisma.loja.create({ data });
   }
+
+  update(id: string, dto: UpdateLojaDto): Promise<Loja> {
+    const data: Partial<Loja> = { ...dto };
+
+    return this.prisma.loja.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.prisma.loja.delete({ where: { id } });
+  } // como não tem retorno, é necessário por um async na função
 }
