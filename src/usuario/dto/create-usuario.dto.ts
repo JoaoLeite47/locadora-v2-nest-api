99c4d3b5e9725424f0883dbe2a1035d89ac5a049
JoaoLeite47 <StalkerJJ@outlook.com>
2022-11-04 16:23:47 -0300

    feat<usuario> all endpoints has implemented

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,1 +1,42 @@
-export class CreateUsuarioDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength, Matches, IsUrl } from 'class-validator';
+
+export class CreateUsuarioDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome de usuário. Utilizado no login. Deve ser único',
+    example: 'Joao_Leite',
+  })
+  nickname: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do usuário. Apenas para exibição',
+    example: 'Joao Leite',
+  })
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha do usuário para login',
+    example: 'N3w_password',
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'A confirmação da senha deve ser igual a senha',
+    example: 'N3w_password',
+  })
+  confirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem de perfil do usuário',
+    example: 'https://avatars.githubusercontent.com/u/100146681?v=4',
+  })
+  image: string;
+}
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(private readonly prisma: PrismaService) {}
+
+  findAll(): Promise<Usuario[]> {
+    return this.prisma.usuario.findMany();
   }
 
-  findAll() {
-    return `This action returns all usuario`;
+  async findById(id: string): Promise<Usuario> {
+    const record = await this.prisma.usuario.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Usuario com o ID '${id}' não encontrado!`);
+    } // caso o registro não seja encontrado, ele retorna um 404 not found
+
+    return record;
+  } // metodo para funções que utilizam o id para tratamento de error 404
+
+  async findOne(id: string): Promise<Usuario> {
+    return this.findById(id);
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} usuario`;
+  create(dto: CreateUsuarioDto): Promise<Usuario> {
+    delete dto.confirmPassword;
+    const data: Usuario = { ...dto };
+    return this.prisma.usuario.create({ data }).catch(this.handleError);
   }
 
-  update(id: string, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
+  handleError(error: Error): undefined {
+    const errorLines = error.message?.split('\n'); // vai pegar as queblas de linhas do erro e separar a parte que me interessa
+    const lastErrorLine = errorLines[errorLines.length - 1]; // me tras a ultima linha do erro, na qual o erro está melhor descrito
+    throw new UnprocessableEntityException(
+      lastErrorLine || 'Algum erro aconteceu na operação',
+    );
+  } // function para satisfazer o erro de criação de usuario com number duplicado
+
+  async update(id: string, dto: UpdateUsuarioDto): Promise<Usuario> {
+    await this.findById(id);
+
+    delete dto.confirmPassword;
+
+    const data: Partial<Usuario> = { ...dto };
+
+    return this.prisma.usuario
+      .update({
+        where: { id },
+        data,
+      })
+      .catch(this.handleError);
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} usuario`;
-  }
+  async remove(id: string) {
+    await this.findById(id);
+    await this.prisma.usuario.delete({ where: { id } });
+  } // como não tem retorno, é necessário por um async na função
 }
